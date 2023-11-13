f9dd20da9804fc31420beb9536567ae8a1164b1e
Philip <56880827+ajemphilip@users.noreply.github.com>
2023-11-13 16:22:26 -0500

    asda

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import axios from 'axios'
 import ResultOutput from './components/ResultOutput';
 import Form from './components/Form';
